c3e4e40185361b37c2a13c5e0b74a93713b3aa88
Edgar920627 <72446789+Edgar920627@users.noreply.github.com>
2020-10-11 00:43:22 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,32 +5,38 @@ PROJECT_NAME=Gradle_zoomEdgar
 WAR_FILE_NAME=zoomEdgar.war
 TOMCAT_DEPLOY=/usr/share/tomcat8/webapps
 
-LOG=$REPOSITORY/logs/tomcat_restart.log
-DATE=`Y%m%d-%H%M%S`
+LOG=$REPOSITORY/logs/restart.log
+DATE=`date +%Y/%m/%d-%H:%M:%S`
 
 cd $REPOSITORY
-echo "> CodeDepoly Start $DATE" >> $LOG
-echo "> War File Chown Tomcat" >> $LOG
+echo "CodeDepoly Tomcat Start $DATE" >> $LOG
 
-sudo chown tomcat:tomcat $REPOSITORY/zip/$WAR_FILE_NAME
-
-echo "> Build file Tomcat Copy" >> $LOG
-
-sudo cp $REPOSITORY/zip/$WAR_FILE_NAME $TOMCAT_DEPLOY
+echo -e  >> $LOG
 
 TOMCAT_PID=`ps -ef | grep tomcat | grep -v grep | grep -v vi | awk '{print $2}'`
 
-TOMCAT_CNT=`ps -ef | grep tomcat | grep -v grep | grep -v vi | wc -l`
+echo "[1] cuurent pid : $TOMCAT_PID" >> $LOG
 
-echo "> Tomcat PID : $TOMCAT_PID" >> $LOG
-echo "> Tomcat CNT : $TOMCAT_CNT" >> $LOG
+echo "[2] stop"  >> $LOG
+sudo service tomcat8 stop
 
-if [ $TOMCAT_CNT -gt 0 ]
-then    
-    echo "> TOMCAT을 종료하고 재시작 합니다.(PID : $TOMCAT_PID)" >> $LOG
-    sudo service tomcat8 stop
-    sudo service tomcat8 start
-else
-    echo "> TOMCAT을 시작합니다" >> $LOG
-    sudo service tomcat8 start
-fi
+echo "[3] war copy" >> $LOG
+sudo cp $REPOSITORY/zip/$WAR_FILE_NAME $TOMCAT_DEPLOY
+
+echo "[4] war chown" >> $LOG
+sudo chown tomcat:tomcat $TOMCAT_DEPLOY/$WAR_FILE_NAME
+
+echo "[5] start" >> $LOG
+sudo service tomcat8 start
+
+TOMCAT_PID=`ps -ef | grep tomcat | grep -v grep | grep -v vi | awk '{print $2}'`
+echo "[6] restart PID : $TOMCAT_PID" >> $LOG
+
+echo -e  >> $LOG
+
+DATE=`date +%Y/%m/%d-%H:%M:%S`
+
+echo "CodeDepoly Tomcat End   $DATE" >> $LOG
+
+RESULT="\n\n"
+echo -e $RESULT >> $LOG
