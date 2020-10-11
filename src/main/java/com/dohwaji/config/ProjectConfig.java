a58e092b100ce27905ef469f459e76b1f1e49224
7dnjs <7dnjs@DESKTOP-J00PGRH>
2020-10-11 16:20:26 +0900

    첨부파일

--- src/main/java/com/dohwaji/config/ProjectConfig.java
+++ src/main/java/com/dohwaji/config/ProjectConfig.java
@@ -6,9 +6,9 @@ public class ProjectConfig {
 	 * 서버의 상품이미지 업로드 경로( 나중에 서버 경로 여기에서 수정하면 됨 )
 	 * 테스트시에는 자기 로컬 디렉토리가 있어야함
 	 */
-	public static String BBS_UPLOAD_LOCATION = "C:\\asd\\zoom\\WebContent\\app\\bbs\\bbs_images";
+	public static String BBS_UPLOAD_LOCATION = "/home/ec2-user/app/uploads";
 	public static String PRODUCT_UPLOAD_LOCATION = "/home/ec2-user/app/uploads";
-	public static String USER_UPLOAD_LOCATION = "C:\\JSP_01\\test_dd\\WebContent\\blue\\imagesProduct";
-	public static String BTTIP_UPLOAD_LOCATION = "C:\\JSP_01\\test_dd\\WebContent\\blue\\imagesProduct";
+	public static String USER_UPLOAD_LOCATION = "/home/ec2-user/app/uploads";
+	public static String BTTIP_UPLOAD_LOCATION = "/home/ec2-user/app/uploads";
 	//String saveFolder = request.getServletContext().getRealPath("");
 }
